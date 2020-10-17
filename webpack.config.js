3dd2d810168c3a55c357b1ea03b3313014e7d87e
de1ain <borodin.tim@gmail.com>
2020-10-17 17:02:07 +0300

    Adding styling - login page, header

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,8 @@ module.exports = (env) => {
                     }, {
                         loader: 'css-loader',
                         options: {
-                            sourceMap: true
+                            sourceMap: true,
+                            url: false
                         }
                     }, {
                         loader: 'sass-loader',
