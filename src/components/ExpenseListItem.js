4e012e6f998a3f486806411cab201f7953010e0f
de1ain <borodin.tim@gmail.com>
2020-10-19 13:50:31 +0300

    Changed currency symbol to NIS. Updated tests. Changed Login page background.

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -9,7 +9,7 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
             <h3 className="list-item__title">{description}</h3>
             <span className="list-item__sub-title">{moment(createdAt).format('DD MMM YYYY')}</span>
         </div>
-        <h3 className="list-item__data">{numeral(amount / 100).format('$0,0.00')}</h3>
+        <h3 className="list-item__data">₪{numeral(amount / 100).format('0,0.00')}</h3>
     </Link>
 );
 
