af348f64d3d8093181ac9e00b892e84806efd209
de1ain <borodin.tim@gmail.com>
2020-10-19 14:43:09 +0300

    Changed the currency symbol to NIS on the expenses summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -7,13 +7,13 @@ import selectExpenses from '../selectors/expenses';
 
 export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
     const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
-    const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+    const formattedExpensesTotal = numeral(expensesTotal / 100).format('0,0.00');
 
     return (
         <div className="page-header">
             <div className="content-container">
                 <h1 className="page-header__title">
-                    Viewing <span>{expenseCount}</span> {expenseWord} totalling: <span>{formattedExpensesTotal}</span>
+                    Viewing <span>{expenseCount}</span> {expenseWord} totalling: <span>₪{formattedExpensesTotal}</span>
                     <div className="page-header__actions">
                         <Link className="button" to="/create">Add Expense</Link>
                     </div>
