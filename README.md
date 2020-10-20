b2960e73ae0c73aac68012f9aa079f0e6025babb
de1ain <borodin.tim@gmail.com>
2020-10-20 23:10:44 +0300

    Added display of hidden expenses count on Expenses Summary

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ To push changes to heroku: git push heroku master
 
 
 TODOs:
-1. Add confirmation modal when removing expense
-2. Show number of hidden expenses in /dashboard summary
+1. [DONE] Add confirmation modal when removing expense
+2. [DONE] Show number of hidden expenses in /dashboard summary
     - Add button to show all expenses
 3. Add support for another social login system (GitHub, Facebook, Twitter)
 4. Buy a custom domain and configure it with Heroku (or use subdomain on owned domain)
--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -5,19 +5,22 @@ import numeral from 'numeral';
 import selectExpensesTotal from '../selectors/expenses-total';
 import selectExpenses from '../selectors/expenses';
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
-    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+export const ExpensesSummary = ({ expenseCount, expensesTotal, hiddenExpenseCount }) => {
+    const expenseWord = (expenseCount === 1) ? 'expense' : 'expenses';
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('0,0.00');
+    const existWord = (hiddenExpenseCount === 0 || hiddenExpenseCount === 1) ? 'is' : 'are';
+    const pluralS = (hiddenExpenseCount === 1) ? '' : 's';
 
     return (
         <div className="page-header">
             <div className="content-container">
                 <h1 className="page-header__title">
                     Viewing <span>{expenseCount}</span> {expenseWord} totalling: <span>₪{formattedExpensesTotal}</span>
-                    <div className="page-header__actions">
-                        <Link className="button button--hover-darken" to="/create">Add Expense</Link>
-                    </div>
                 </h1>
+                <p className="page-header__title">In addition, there {existWord} <span>{hiddenExpenseCount}</span> hidden expense{pluralS}</p>
+                <div className="page-header__actions">
+                    <Link className="button button--hover-darken" to="/create">Add Expense</Link>
+                </div>
             </div>
         </div>
     );
@@ -25,9 +28,11 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
 
 const mapStateToProps = (state) => {
     const visibleExpenses = selectExpenses(state.expenses, state.filters);
+    const hiddenExpenses = state.expenses.length - visibleExpenses.length;
     return {
         expenseCount: visibleExpenses.length,
-        expensesTotal: selectExpensesTotal(visibleExpenses)
+        expensesTotal: selectExpensesTotal(visibleExpenses),
+        hiddenExpenseCount: hiddenExpenses
     };
 };
 
