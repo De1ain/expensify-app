bae147b177a7718dc04f3edb2e9f971d4ae3fb0c
de1ain <borodin.tim@gmail.com>
2020-10-21 18:29:09 +0300

    Adding authentication with Github. Added Facebook authbutton, auth with FB is disabled. New button styles added for disabled buttons

--- README.md
+++ README.md
@@ -12,5 +12,8 @@ TODOs:
 1. [DONE] Add confirmation modal when removing expense
 2. [DONE] Show number of hidden expenses in /dashboard summary
     - Add button to show all expenses
-3. Add support for another social login system (GitHub, Facebook, Twitter)
+3. Add support for another social login system
+    [DONE]  GitHub
+            Facebook
+            Twitter
 4. Buy a custom domain and configure it with Heroku (or use subdomain on owned domain)
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { startLogin } from '../actions/auth';
+import { startGoogleLogin, startFacebookLogin, startGithubLogin } from '../actions/auth';
 
-export const LoginPage = ({ startLogin }) => (
+export const LoginPage = ({ startGoogleLogin, startGithubLogin }) => (
     <div className="box-layout">
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expensify</h1>
             <p>It's time to get your expenses under control</p>
-            <button className="button button--shadow" onClick={startLogin}>
-                <span className="button--icon"></span>
+            <button className="button button--shadow button--login" onClick={startGoogleLogin}>
+                <img src="/images/google-icon.jpeg" className="button--icon" />
                 <span>Login with Google</span>
+            </button>
+            <button className="button button--shadow button--login" onClick={startFacebookLogin} disabled>
+                <img src="/images/facebook-icon.jpg" className="button--icon" />
+                <span>Login with Facebook</span>
+            </button> 
+            <button className="button button--shadow button--login" onClick={startGithubLogin}>
+                <img src="/images/github-icon.png" className="button--icon" />
+                <span>Login with GitHub</span>
             </button>
         </div>
     </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
-    startLogin: () => dispatch(startLogin())
+    startGoogleLogin: () => dispatch(startGoogleLogin()),
+    startGithubLogin: () => dispatch(startGithubLogin())
 });
 
 export default connect(undefined, mapDispatchToProps)(LoginPage);
