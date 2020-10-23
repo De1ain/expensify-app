67f90c485ed1c4bd8922810f92e41c4ddea38971
de1ain <borodin.tim@gmail.com>
2020-10-23 13:13:17 +0300

    Removed the disabled Facebook login button. Changed icon for Github login button

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,16 +7,12 @@ export const LoginPage = ({ startGoogleLogin, startGithubLogin }) => (
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expensify</h1>
             <p>It's time to get your expenses under control</p>
-            <button className="button button--shadow button--login" onClick={startGoogleLogin}>
-                <img src="/images/google-icon.jpeg" className="button--icon" />
+            <button className="button button--shadow button--login button--google" onClick={startGoogleLogin}>
+                <img className="button--icon" src="/images/google-icon.jpeg" />
                 <span>Login with Google</span>
             </button>
-            <button className="button button--shadow button--login" onClick={startFacebookLogin} disabled>
-                <img src="/images/facebook-icon.jpg" className="button--icon" />
-                <span>Login with Facebook</span>
-            </button> 
-            <button className="button button--shadow button--login" onClick={startGithubLogin}>
-                <img src="/images/github-icon.png" className="button--icon" />
+            <button className="button button--shadow button--login button--github" onClick={startGithubLogin}>
+                <img className="button--icon" src="/images/github-icon-darkBg.png" />
                 <span>Login with GitHub</span>
             </button>
         </div>
@@ -25,6 +21,7 @@ export const LoginPage = ({ startGoogleLogin, startGithubLogin }) => (
 
 const mapDispatchToProps = (dispatch) => ({
     startGoogleLogin: () => dispatch(startGoogleLogin()),
+    startFacebookLogin: () => dispatch(startFacebookLogin()),
     startGithubLogin: () => dispatch(startGithubLogin())
 });
 
